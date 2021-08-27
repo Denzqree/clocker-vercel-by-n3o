1cec18d4af25b667d969477733111a4d1cabc974
denzqree <valterdaniel.madeira@gmail.com>
2021-08-27 01:13:26 +0100

    feature: login and signup with home

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
+// https://stackoverflow.com/questions/55175445/cant-import-svg-into-next-js
+
 module.exports = {
+  webpack5: false,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"]
+      issuer: {
+        test: /\.(js|ts)x?$/,
+       // for webpack 5 use
+       // { and: [/\.(js|ts)x?$/] }
+      },
+      
+      use: ['@svgr/webpack'],
     });
 
     return config;
-  }
+  },
 };
--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,17 @@
-import { useFormik } from 'formik';
-
-import * as yup from 'yup';
+import Link from 'next/link';
 
 import { 
   Container, 
   Box, 
-  Input, 
   Button, 
-  Text, 
-  FormControl, 
-  FormLabel, 
-  FormHelperText,
-  InputLeftAddon,
-  InputGroup,
+  Text
 } from '@chakra-ui/react';
 
-import { Logo } from './../components/Logo';
+import { Logo } from '../components/Logo';
 
-import firebase from './../config/firebase';
-
-const validationSchema = yup.object().shape({
-  email: yup.string().email('Email inválido').required('Preenchimento obrigatório'),
-  password: yup.string().required('Preenchimento obrigatório'),
-  username: yup.string().required('Preenchimento obrigatório'), //Pode/deve se verificar se o usuario ja esta cadastrado
-});
+import firebase from '../config/firebase';
 
 export default function Home() {
-  const {
-    values, 
-    errors, 
-    touched, 
-    handleChange, 
-    handleBlur, 
-    handleSubmit,
-    isSubmitting,
-  } = useFormik({
-    onSubmit: (values, form) => {
-      console.log(values);
-    },
-    validationSchema,
-    initialValues: {
-      email: '',
-      username: '',
-      password: '',
-    }
-  })
   return (
     <Container p={4} centerContent>
 
@@ -54,31 +21,15 @@ export default function Home() {
         <Text>Crie sua agenda compartilhada</Text>
       </Box>
 
-      <Box p={4} width="100%" >
-        <FormControl id="email" isRequired>
-          <FormLabel>Email</FormLabel>
-          <Input size="lg" type="email" value={values.email} onChange={handleChange} onBlur={handleBlur} />
-          {touched.email && <FormHelperText textColor="#e74c3c">{errors.email}</FormHelperText> }
-        </FormControl>
-        
-        <FormControl id="password" isRequired>
-          <FormLabel>Senha</FormLabel>
-          <Input size="lg" type="password" value={values.password} onChange={handleChange} onBlur={handleBlur} />
-          {touched.password && <FormHelperText textColor="#e74c3c">{errors.password}</FormHelperText> }
-        </FormControl>
+
+      <Box p={4,2} mt={8} width="100%">
+        <Link href="/login"><Button width="100%">Login</Button></Link>
       </Box>
 
-      <FormControl id="username" p={4} isRequired>
-        <InputGroup>
-          <InputLeftAddon children="n3o.pt/clocker/"/>
-          <Input type="username" value={values.username} onChange={handleChange} onBlur={handleBlur}/>
-        </InputGroup>
-        {touched.username && <FormHelperText textColor="#e74c3c">{errors.username}</FormHelperText> }
-      </FormControl>
-
-      <Box p={4}>
-        <Button width="100%" onClick={handleSubmit} colorScheme="blue" isLoading={isSubmitting}>Entrar</Button>
+      <Box p={4,2} width="100%">
+      <Link href="/signup"><Button width="100%" colorScheme="green">Cadastrar-se</Button></Link>
       </Box>
+
     </Container>
   )
 };
