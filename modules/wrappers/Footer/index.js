607fb65ff6aa39facbda6c26dee43742947981d1
denzqree <valterdaniel.madeira@gmail.com>
2021-09-01 07:53:38 +0100

    fix: removed position:relative for footer, mobile problem

--- modules/wrappers/Footer/index.js
+++ modules/wrappers/Footer/index.js
@@ -2,7 +2,7 @@ import { Container, Box, Text } from '@chakra-ui/react'
 
 export const Footer = ({ children }) => {
     return (
-        <Box backgroundColor="#f7f7f7" width="100vw" height="65px" zIndex="2" textAlign="center" paddingY={2} marginTop="-65px" position="relative">
+        <Box backgroundColor="#f7f7f7" width="100vw" height="65px" zIndex="2" textAlign="center" paddingY={2} marginTop="-65px">
                 <Text>{ children }</Text>
         </Box>
     )
