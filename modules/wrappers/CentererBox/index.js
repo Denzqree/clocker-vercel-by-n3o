0e1feeae682ef98476b0c510ad1148580055ab20
denzqree <valterdaniel.madeira@gmail.com>
2021-09-01 07:48:41 +0100

    refactor: removed border (used for dev purposes)

--- modules/wrappers/CentererBox/index.js
+++ modules/wrappers/CentererBox/index.js
@@ -2,7 +2,7 @@ import { Box } from '@chakra-ui/react'
 
 export const CentererBox = ({ children }) => {
   return (
-    <Box borderWidth="1px" borderColor="blue" backgroundColor="white" display="flex" justifyContent="center">
+    <Box backgroundColor="white" display="flex" justifyContent="center">
       {children}
     </Box>
   );
